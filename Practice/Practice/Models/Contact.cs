449a01442199aaa469af64d912bc1688798b7a29
DELL <duongquocv@gmail.com>
2021-12-20 16:20:28 +0700

    practice

---
+++ Practice/Practice/Models/Contact.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Practice.Models
+{
+    public class Contact
+    {
+        [Key]
+        public int id { get; set; }
+        [Required]
+        public string ContactName { get; set; }
+        [Required(ErrorMessage = "Vui lòng nhập tên liên lạc")]
+        public int ContactNumber { get; set; }
+        public string GroupName { get; set; }
+        public string HireDate { get; set;  }
+
+        public string Birthday { get; set; }
+    }
+}
---
+++ Practice/Practice/Models/DataContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace Practice.Models
+{
+    public class DataContext : DbContext
+    {
+        
+            public DataContext() : base("Practice") { }
+
+            public DbSet<Contact> Contacts { get; set; }
+
+            
+        
+    }
+}
